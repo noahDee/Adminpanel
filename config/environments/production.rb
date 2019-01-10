75fc52e2995b9632ec6e952232ce2ed5e2adc962
noah <you@example.com>
2019-01-10 15:02:59 -0500

    wip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,11 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  if ENV['RACK_ENV']
+    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+  else
+    set :database, {adapter: "sqlite3", database: "database.sqlite3"}
+  end
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
@@ -89,11 +94,6 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  if ENV['RACK_ENV']
-    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-  else
-    set :database, {adapter: "sqlite3", database: "database.sqlite3"}
-  end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
