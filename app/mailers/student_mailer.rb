dd4f4ab390bbdc09c871414b8652cfea40080bcb
noah <you@example.com>
2019-01-10 15:30:57 -0500

    broken

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -1,10 +1,10 @@
 class StudentMailer < ApplicationMailer
-  def welcome_email(student)
-    recipients "#{student.email}"
-    from        "Baking School <noah.dorce65@gmail.com>"
-    subject     "Welcome Student"
-    sent_on     Time.now
-    body        {:student => student}
-    content_type "text/html"
-  end
+  # def welcome_email(student)
+  #   recipients "#{student.email}"
+  #   from        "Baking School <noah.dorce65@gmail.com>"
+  #   subject     "Welcome Student"
+  #   sent_on     Time.now
+  #   body        {:student => student}
+  #   content_type "text/html"
+  # end
 end
